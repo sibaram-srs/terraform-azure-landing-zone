84af2e4921575b2015dccc46d7c58d0aa5ce3a19
Sibaram S <sibaram917@gmail.com>
2026-06-11 15:29:46 +0530

    Update outputs.tf

--- resource_group/outputs.tf
+++ resource_group/outputs.tf
@@ -1,18 +1,10 @@
-# output "resource_group_names" {
-#   value = {
-#     for key, rg in azurerm_resource_group.rg :
-#     key => rg.name
-#   }
-# }
+ output "resource_group_names" {
+   value = {
+     for key, rg in azurerm_resource_group.rg :
+     key => rg.name
+   }
+ }
 
 # output "rg-group" {
 #   value = azurerm_resource_group.rg1
 # }
-
-output "rg-group" {
-  value = azurerm_resource_group.rg.name
-}
-
-output "rg-group-id" {
-  value = azurerm_resource_group.rg.id
-}
