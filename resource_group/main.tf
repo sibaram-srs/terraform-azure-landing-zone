ff1daf85b8c7f5dee94fc07bda98f7d9d8a6096b
Sibaram S <sibaram917@gmail.com>
2026-06-11 15:29:10 +0530

    Update main.tf

--- resource_group/main.tf
+++ resource_group/main.tf
@@ -1,11 +1,5 @@
-# resource "azurerm_resource_group" "rg" {
-#   for_each = var.resource_groups
-#   name     = each.value.name
-#   location = each.value.location
-# }
-
-
-resource "azurerm_resource_group" "rg" {
-  name     = "sr-rg"
-  location = "West Europe"
-}
+ resource "azurerm_resource_group" "rg" {
+   for_each = var.resource_groups
+   name     = each.value.name
+   location = each.value.location
+ }
